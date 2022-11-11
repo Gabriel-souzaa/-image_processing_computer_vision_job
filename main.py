a7bb935a4eb4e75367dc299ab99d67491ccd8847
Gabriel-souzaa <gabrielfreitassouzaa@gmail.com>
2022-11-11 02:32:32 -0300

    chore: Calculo de imagens

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFilter
+import cv2
 
 #Ler imagem
 im = Image.open( './images/eu.jpg' )
@@ -17,7 +18,21 @@ find_edhes_kernel_one = find_edhes.filter( ImageFilter.Kernel((3,3), (1,0,-1,2,0
 find_edhes_kernel_two = find_edhes.filter( ImageFilter.Kernel((3,3), (1,2,1,0,0,0,-1,-2,-1),1,0) )
 
 #Salvando a imagem filtradas
+shade_gray.save( './images/shade_gray.jpg', 'JPEG' )
 blur_kernel.save( './images/image_blur.jpg', 'JPEG' )
 sharpen.save( './images/image_sharpen.jpg', 'JPEG' )
 find_edhes_kernel_one.save('./images/image_find_edhes_1.jpg', 'JPEG')
 find_edhes_kernel_two.save('./images/image_find_edhes_2.jpg', 'JPEG')
+
+#Calculo de imagens
+im_blur = cv2.imread('./images/image_blur.jpg')
+im_shade_gray = cv2.imread('./images/shade_gray.jpg')
+
+#Subtraindo e criando  imagem orignal pelo filtro de blur
+unsharp = cv2.subtract(im_shade_gray, im_blur)
+cv2.imwrite("./images/unsharp.jpg", unsharp)
+
+#Juntando e criando imagem original com o unsharp
+im_unsharp = cv2.imread('./images/unsharp.jpg')
+sharpening = cv2.add(im_unsharp, im_shade_gray)
+cv2.imwrite("./images/sharpening.jpg", sharpening)
